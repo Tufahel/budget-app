91fb5b43bdd48d3629f4fcbbb43927faadb6bf1e
Tufahel <tufahel96@gmail.com>
2022-07-04 23:12:47 +0600

    controller setup

---
+++ app/controllers/entities_controller.rb
@@ -0,0 +1,14 @@
+class EntitiesController < ApplicationController
+    def index
+    end
+    def new
+    end
+    def show
+    end
+    def create
+    end
+    def destroy
+    end
+    def entity_params
+    end
+end
---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,14 @@
+class GroupsController < ApplicationController
+    def index
+    end
+    def new
+    end
+    def show
+    end
+    def create
+    end
+    def destroy
+    end
+    def group_params
+    end
+end
