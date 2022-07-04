da04b857ca70aab6b1342ecc370ded0dffaf3d3e
Tufahel <tufahel96@gmail.com>
2022-07-04 23:13:56 +0600

    linters fixed

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,13 @@
 class GroupsController < ApplicationController
-    def index
-    end
-    def new
-    end
-    def show
-    end
-    def create
-    end
-    def destroy
-    end
-    def group_params
-    end
+  def index; end
+
+  def new; end
+
+  def show; end
+
+  def create; end
+
+  def destroy; end
+
+  def group_params; end
 end
