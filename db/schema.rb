6bf24fa96ba858581856caf494ad1594d17a18f2
Tufahel <tufahel96@gmail.com>
2022-07-05 23:34:28 +0600

    setup devise authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_05_163136) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_05_172321) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_05_163136) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_categories_on_author_id"
+  end
+
+  create_table "categories_deals", id: false, force: :cascade do |t|
+    t.bigint "deal_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id", "deal_id"], name: "index_categories_deals_on_category_id_and_deal_id"
+    t.index ["deal_id", "category_id"], name: "index_categories_deals_on_deal_id_and_category_id"
   end
 
   create_table "deals", force: :cascade do |t|
@@ -36,6 +43,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_05_163136) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "categories", "users", column: "author_id"
