e193a6a46ea7deb1639b6e3d734a04dbab47e6d9
Tufahel <tufahel96@gmail.com>
2022-07-06 20:04:10 +0600

    users controller refactor and splash controller added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,6 @@ class UsersController < ApplicationController
   load_and_authorize_resource
   def index; end
 
-  def new; end
-
   def show; end
 
-  def create; end
-
-  def destroy; end
-
-  def user_params; end
 end
